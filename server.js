0928ba236441278de8e91f340457174453275b51
Jose Starsky Naya Lara <starsky@Joses-MacBook-Pro.local>
2017-04-15 08:28:03 +0200

    Add project page

--- server.js
+++ server.js
@@ -59,6 +59,12 @@ app.get('/bad', (req, res) => {
     })
 });
 
+app.get('/projects', (req, res) => {
+    res.render('projects.hbs', {
+            pageTitle: 'Projects Page'
+    });
+});
+
 app.listen(port, () => {
     console.log('Server is up on port 3000')
 });
